d950eadb2f010ff8b70ee052fe1251edf23c889a
Dhrub Kumar <dhrub0216@gmail.com>
2020-07-08 21:10:45 +0530

    2st

--- app.js
+++ app.js
@@ -1,8 +1,63 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+const User= {
+    email : {
+        type:String,
+        unique: true,
+        required: true,
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error ('Email is invalid')
+            }
+        }
+
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    }
+}
+
+const findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+app.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
 
 
 
