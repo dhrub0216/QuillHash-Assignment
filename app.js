90b8287ac456d31f905729d140a62f1c16dfa67f
Dhrub Kumar <dhrub0216@gmail.com>
2020-07-10 12:26:46 +0530

    Running version 1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mongoose= require('mongoose')
-//var validator= require('validator')
-//var fs= require('fs')
+var validator= require('validator')
+var fs= require('fs')
 var app = express();
 var port=3000;
 app.use(express.json())
@@ -15,11 +15,11 @@ var userSchema= new mongoose.Schema({
         type: 'String',
         required: true,
         trim: true,
-        // validate(email){ 
-        //     if (!validator.isEmail(email)){
-        //         return ("Your Email Id is not correct")
-        //     }
-        // }
+        validate(email){ 
+            if (!validator.isEmail(email)){
+                return ("Your Email Id is not correct")
+            }
+        }
     },
     password: {
         type: 'String',
@@ -29,14 +29,14 @@ var userSchema= new mongoose.Schema({
     }
 })
 
-// const readNginx= function(){
-//     fs.readFile('./tmp/directory/nginx.log', function(err,data){
-//         if(err){
-//             throw err
-//         }
-//         console.log(data)
-//     })
-//     }
+const readNginx= function(){
+    fs.readFile('./tmp/directory/nginx.log', function(err,data){
+        if(err){
+            throw err
+        }
+        console.log(data.toString())
+    })
+    }
 
 app.post('/login',function(req,res){
 const User= mongoose.model('User',userSchema)
@@ -44,19 +44,12 @@ const x= req.body
 const user= new User(x)
 try{
     user.save()
-  //  readNginx()
-    res.status(200).send(user.name,user.password)
+    readNginx()
+    res.status(200).send(user)
 } catch (e) {
     res.status(404)
 }
 })
-
-
-
-
-
-
-
 
 app.listen(port,function(){
     console.log('Server running on port '+port);
