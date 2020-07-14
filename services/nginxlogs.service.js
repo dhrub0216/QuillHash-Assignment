6746757274befeba776ab79cab8098026e057758
Dhrub Kumar <dhrub0216@gmail.com>
2020-07-14 15:56:22 +0530

    Filtering feature

--- services/nginxlogs.service.js
+++ services/nginxlogs.service.js
@@ -1,14 +1,15 @@
 
 const fs= require('fs')
 const re= require('re')
- exports.Fetchlogs= function(body ,callback){
-        fs.readFile('./tmp/directory/nginx.log', function(err,data){
-            if(err){
-               callback(err)
-            }
-            callback(null,data.toString())
-        })  
- }
+const rl= require('readline')
+//  exports.Fetchlogs= function(body ,callback){
+//         fs.readFile('./tmp/directory/nginx.log', function(err,data){
+//             if(err){
+//                callback(err)
+//             }
+//             callback(null,data.toString())
+//         })  
+//  }
 
  exports.Filterlogsbyip= function(body,callback){
    fs.readFile('./tmp/directory/nginx.log', function(err,data){
@@ -43,3 +44,49 @@ exports.Filterlogsbydate= function(body,callback){
      callback(null,data.toString().match(/(0?[1-9]|[12][0-9]|3[01])[\/\-]([a-zA-Z]*)[\/\-]\d{4}:([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])/g ))
 })
 }
+
+
+exports.Filterlogsbydate= function(body,callback){
+   fs.readFile('./tmp/directory/nginx.log', function(err,data){
+      if(err){
+         callback(err)
+      }
+   var dateHash = {
+      Jan : 1,
+      Feb: 2,
+      Mar: 3,
+      Apr: 4,
+      May: 5,
+      Jun: 6,
+      Jul: 7,
+      Aug: 8,
+      Sep: 9,
+      Oct: 10,
+      Nov: 11,
+      Dec: 12
+     };
+     callback(null,data.toString().match(/(0?[1-9]|[12][0-9]|3[01])[\/\-]([a-zA-Z]*)[\/\-]\d{4}:([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])/g ))
+})
+}
+
+var master=[];
+var variety=[];
+exports.Fetchlogs= function(body,callback){
+var lineReader = rl.createInterface({
+   input: fs.createReadStream('./tmp/directory/nginx.log'),
+   output: console.log(master)
+ });
+ 
+ lineReader.on('line', function (line) {
+   variety.push(line);
+   matched_ip= line.match(/([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]).([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]).([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]).([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]).([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])/g)
+   variety.push(matched_ip)
+   matched_date= line.match(/(0?[1-9]|[12][0-9]|3[01])[\/\-]([a-zA-Z]*)[\/\-]\d{4}/g )
+   variety.push(matched_date)
+   master.push(variety)
+
+})
+
+
+callback(null, master)
+} 
